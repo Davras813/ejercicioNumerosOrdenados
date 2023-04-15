4d74a4c5d728bd936440fa91264b26fa39d49002
Davras813 <davidloaiza13@gmail.com>
2023-04-14 21:38:10 -0500

    Carga de codigo javascript arreglo

--- assets/js/logica.js
+++ assets/js/logica.js
@@ -1,20 +1,17 @@
 
 function ordenarNumeros() {
     let lista = [23, 45, 78, 32, 12, 1, 4, 8, 34, 32, 1000, 34, 37, 22, 0, -4, -6]
-
+    console.log(lista)
     for (let index = 0; index < lista.length; index++) {
-        const element = lista[index];
         for (let index2 = 0; index2 < lista.length; index2++) {
             if (lista[index] < lista[index2]) {
                 let aux = lista[index]
                 lista[index] = lista[index2]
                 lista[index2] = aux
-
             }
         }
-
+        console.log(lista)
     }
-    console.log(lista)
 }
 
 ordenarNumeros()
